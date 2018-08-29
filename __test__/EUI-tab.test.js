17579a46a752698100f8a688ae98556e434c4ec4
pengzhan <pengzhan@mistong.com>
2018-08-29 11:23:16 +0800

    fix(fix export): fix

--- __test__/EUI-tab.test.js
+++ __test__/EUI-tab.test.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-14';
 // import EmptyData from '../src/';
-import { Tabs, TabPane } from '../src/tabs';
+import { Tabs, TabPane } from '../src/index';
 
 Enzyme.configure({ adapter: new Adapter() });
 
